394e8bc2d28e8b34fb1f6c32c46d3aa1b6318d50
kaysanshifan <kaysanshifan@gmail.com>
2025-02-05 14:22:48 +0700

    halaman login

---
+++ lib/login.dart
@@ -0,0 +1,39 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'login_widget.dart' show LoginWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class LoginModel extends FlutterFlowModel<LoginWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for emailAddress widget.
+  FocusNode? emailAddressFocusNode;
+  TextEditingController? emailAddressTextController;
+  String? Function(BuildContext, String?)? emailAddressTextControllerValidator;
+  // State field(s) for password widget.
+  FocusNode? passwordFocusNode;
+  TextEditingController? passwordTextController;
+  late bool passwordVisibility;
+  final passwordMask = MaskTextInputFormatter(mask: '........');
+  String? Function(BuildContext, String?)? passwordTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {
+    passwordVisibility = false;
+  }
+
+  @override
+  void dispose() {
+    emailAddressFocusNode?.dispose();
+    emailAddressTextController?.dispose();
+
+    passwordFocusNode?.dispose();
+    passwordTextController?.dispose();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'login.dart';
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://etyutxvbuosvopeiftgo.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0eXV0eHZidW9zdm9wZWlmdGdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3MTY0ODEsImV4cCI6MjA1NDI5MjQ4MX0.hsYCtaOWuITxQBKnt6k0IktdO788WA-MwXlJq6CuXSw',
+  );
+  runApp(MyApp());
 }
+        
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
